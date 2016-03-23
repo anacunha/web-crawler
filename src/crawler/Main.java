8affcd0395774b08863d8b95f7e79ba5ee619369
Ana Luiza Cunha <mcunha.ana@gmail.com>
2016-03-23 09:26:26 -0400

    Began working on parser

--- src/crawler/Main.java
+++ src/crawler/Main.java
@@ -3,8 +3,12 @@ package crawler;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Task 1: Generating the corpus from raw Wikipedia articles previously downloaded");
         System.out.println("...");
-        new Crawler("https://en.wikipedia.org/wiki/Sustainable_energy");
+        //new Crawler("https://en.wikipedia.org/wiki/Sustainable_energy");
+
+        Parser.parse();
+
     }
 }
--- src/crawler/Parser.java
+++ src/crawler/Parser.java
@@ -1,7 +1,58 @@
 package crawler;
 
-/**
- * Created by ana on 3/22/16.
- */
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.WordUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Parser {
+
+    public static void parse() {
+        try {
+            Files.walk(Paths.get("pages/")).forEach(filePath -> {
+                if (Files.isRegularFile(filePath) && isTextFile(filePath)) {
+                    try {
+                        //System.out.println(filePath.getFileName());
+                        parse(new String(Files.readAllBytes(filePath)));
+                    }
+                    catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void parse(String document) {
+        StringBuilder parsedDoc = new StringBuilder();
+
+        // Remove first line from document (contains the URL)
+        Document doc = Jsoup.parse(removeFirstLine(document));
+
+        // Title
+        parsedDoc.append(doc.select("h1#firstHeading").text()).append("\n");
+
+        // Plain Textual Content
+        parsedDoc.append(doc.select("div#mw-content-text").text());
+
+        System.out.println(parsedDoc.toString());
+    }
+
+    private static boolean isTextFile(Path filePath) {
+        return FilenameUtils.getExtension(filePath.getFileName().toString()).equals("txt");
+    }
+
+    private static String removeFirstLine(String string) {
+        return string.substring(string.indexOf("\n") + 1);
+    }
 }
